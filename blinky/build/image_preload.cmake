7384783276750732a323bf071c3a531c669d4664
HemjalCF <abu.hemjal@connectedfinland.fi>
2024-01-19 00:59:10 +0200

    bkup

---
+++ blinky/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/nrf_sdk_kurssi/blinky/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ blinky/build_1/blinky/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/nrf_sdk_kurssi/blinky/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CMAKE_VERBOSE_MAKEFILE "FALSE" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "D:/nrf_sdk_kurssi/blinky/nrf5340dk_nrf5340_cpuapp.overlay" CACHE INTERNAL "NCS child image controlled")
+set(SYSBUILD "True" CACHE INTERNAL "NCS child image controlled")
+set(SYSBUILD_CACHE "D:/nrf_sdk_kurssi/blinky/build_1/blinky_sysbuild_cache.txt" CACHE INTERNAL "NCS child image controlled")
